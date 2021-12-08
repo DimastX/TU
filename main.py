cc6e33e9319f2dfb62749007f3b9937456df0bce
DimastX <67646279+DimastX@users.noreply.github.com>
2021-12-08 05:38:37 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-def ()
+import matplotlib.pyplot
+
+
+def plot():
+
+    data = (1, 2, 3, 4, 5)
+    fig, simple_chart = matplotlib.pyplot.subplots()
+
+    simple_chart.plot(data)
+    matplotlib.pyplot.show()
+
+
+#main
+plot()
