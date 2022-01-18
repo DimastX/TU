7fe424d16ad25b72a69036dfddf0fbcab9a7609b
Ulchik25 <95826946+Ulchik25@users.noreply.github.com>
2022-01-18 16:51:45 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,14 @@
+import motion
+
+#main
+r = 1 #circle radius
+x_c = abs(1 + r) #circle center x
+y_c = - abs(1 + r) #circle center y
+time = 3#modeling time
+dt = 0.1 #delta time
+w = 2 #w for cos
+n = 100 #number of body points
+body = motion.body_init(x_c, y_c, r, w, n) #body initialisation
+motion.trajectory(time, dt, body, w, n) #trajectory modeling
+
+
